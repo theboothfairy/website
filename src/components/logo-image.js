6aebedbb28d33fcc6ea4108ffa42e30449bdc331
Laurent Curau <laurent@thecodeshed.dev>
2020-03-03 00:08:07 +0000

    Resize logo image

--- src/components/logo-image.js
+++ src/components/logo-image.js
@@ -2,7 +2,7 @@ import React from "react"
 import logoSVG from "../images/theboothfairy-logo.svg"
 
 const LogoImage = () => {
-  return <><img src={logoSVG} alt="The Booth Fairy" /></>
+  return <><img src={ logoSVG } class="logo" alt="The Booth Fairy" /></>
 }
 
 export default LogoImage
