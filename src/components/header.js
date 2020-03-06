6babb25c83c5f62a9c2b2576a1166f0bc6095569
Laurent Curau <laurent@thecodeshed.dev>
2020-03-06 18:35:23 +0000

    Refactor SVG logo styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,11 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import LogoImage from "../components/logo-image"
+import LogoSVG from "../images/theboothfairy-logo.svg"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            textDecoration: `none`,
-          }}
-        >
-          <LogoImage />
-        </Link>
-      </h1>
+  <header>
+    <div className="flex items-center justify-center">
+      <LogoSVG className="m-10" />
     </div>
   </header>
 )
--- src/components/logo-image.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import logoSVG from "../images/theboothfairy-logo.svg"
-
-const LogoImage = () => {
-  return <><img src={ logoSVG } class="logo" alt="The Booth Fairy" /></>
-}
-
-export default LogoImage
